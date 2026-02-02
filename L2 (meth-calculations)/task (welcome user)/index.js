f35367a961715428f5bf36cb25f9ff664c9c01f0
ammarAlzaqam <git@ammaralzaqam.com>
2026-02-02 17:09:32 +0200

    L2 welcome user task

---
+++ L2 (meth-calculations)/task (welcome user)/index.js
@@ -0,0 +1,9 @@
+let username = prompt("Enter your name...");
+let birthday = prompt("Enter your birthday...");
+
+let age = 2026 - birthday;
+
+const resultMsg = "Welcome " + username + ", your age is: " + age;
+
+console.log(resultMsg);
+document.writeln(resultMsg);
