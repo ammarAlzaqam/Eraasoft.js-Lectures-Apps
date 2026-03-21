43336612265bf50025791f46df8f10703a4a2118
ammarAlzaqam <git@ammaralzaqam.com>
2026-03-22 00:21:18 +0200

    L13 async vs sync

---
+++ L13 (Sync-Async)/index.js
@@ -0,0 +1,25 @@
+// Sync Code
+// Backend thread (Ads - Posts - Messenger)
+// console.log("5 Sec Ads");
+// console.log("5 Sec Posts");
+// console.log("5 Sec Messenger");
+
+// ES6 (2013)
+// طابور الجبنة في الهايبر ماركت
+// Async Code
+// Request Data
+// async - Built in
+
+setTimeout(() => {
+  console.log("5 Sec Ads");
+}, 5000);
+
+setTimeout(() => {
+  console.log("5 Sec Posts");
+}, 5000);
+
+setTimeout(() => {
+  console.log("5 Sec Messenger");
+}, 5000);
+
+console.log("Website Will Open After: ??? Sec");
